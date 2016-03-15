3bf3c6d1d1c89f5ed466a9e8eb5c736145aafd1a
64b2b6d12b <64b2b6d12b@users.noreply.github.com>
2016-03-15 12:51:46 +1100

    Update otpknock-client.sh

--- otpknock-client.sh
+++ otpknock-client.sh
@@ -3,7 +3,7 @@
 
 # Replace 198.51.100.50 with IP or FQDN of actual server
 ip="198.51.100.50"
-key=$(openssl enc -aes-256-cbc -d -a -in secret-tst.enc)
+key=$(openssl enc -aes-256-cbc -d -a -in secret.enc)
 otp=$(oathtool --totp -s 60 $key)
 sequence=$(sed 's/./&00 /g; s/ $//' <<< $otp)
 
